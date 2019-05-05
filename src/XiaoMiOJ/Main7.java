37ac6bd936f754b43fa2b11458f18865ae994f43
magicya <794343815@qq.com>
2019-05-05 23:05:13 +0800

    7 第一个缺失正数  hashset方式

---
+++ src/XiaoMiOJ/Main7.java
@@ -0,0 +1,50 @@
+package XiaoMiOJ;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * @program: XiaomiOJ
+ * @description: 第一个缺失正数
+ * @author: Ya
+ * @create: 2019-05-05 20:15
+ **/
+public class Main7 {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        String line;
+        while (scan.hasNextLine()) {
+            line = scan.nextLine().trim();
+            System.out.println(method(line));
+        }
+    }
+
+    /**
+     * 用hashset实现O(n)
+     */
+    private static int method(String line) {
+        String[] ss = line.split(",");
+        Set<Integer> set = new HashSet<>();
+        int max = 0;
+        int cur = 0;
+        for (String s : ss) {
+            cur = Integer.parseInt(s);
+            if (cur > max) {
+                max = cur;
+            }
+            if (cur > 0) {
+                set.add(cur);
+            }
+        }
+
+        for (int i = 1; i <= max; i++) {
+            if (set.contains(i)) {
+                continue;
+            } else {
+                return i;
+            }
+        }
+        return max + 1;
+    }
+}
