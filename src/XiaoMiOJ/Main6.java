66afdd1762e266d089e0bbd4567fc73ac6af11bb
magicya <794343815@qq.com>
2019-05-05 20:04:27 +0800

    6 交叉队列 递归方式

--- src/XiaoMiOJ/Main6.java
+++ src/XiaoMiOJ/Main6.java
@@ -15,8 +15,56 @@ public class Main6 {
         while (scan.hasNextLine()) {
             line = scan.nextLine().trim();
             // please write your code here
+            System.out.println(method(line));
+        }
+    }
 
-            // System.out.println("answer");
+    private static boolean method(String line) {
+        String[] ss = line.split(",");
+        char[] c1 = ss[0].toCharArray();
+        char[] c2 = ss[1].toCharArray();
+        char[] c = ss[2].toCharArray();
+
+        if (c.length != c1.length + c2.length) {
+            return false;
+        }
+        int index = 0, index1 = 0, index2 = 0;
+        return method2(c, c1, c2, 0, 0, 0);
+    }
+
+    /**
+     * 递归方式
+     */
+    private static boolean method2(char[] c, char[] c1, char[] c2, int index, int index1, int index2) {
+        if (index1 >= c1.length) {
+            while (index2 < c2.length) {
+                if (c[index] != c2[index2]) {
+                    return false;
+                }
+                index++;
+                index2++;
+            }
+            return true;
+        }
+        if (index2 >= c2.length) {
+            while (index1 < c1.length) {
+                if (c[index] != c1[index1]) {
+                    return false;
+                }
+                index++;
+                index1++;
+            }
+            return true;
+        }
+        if (c[index] != c1[index1] && c[index] != c2[index2]) {
+            return false;
+        } else if (c[index] == c1[index1] && c[index] == c2[index2]) {
+            return method2(c, c1, c2, index + 1, index1 + 1, index2)
+                    || method2(c, c1, c2, index + 1, index1, index2 + 1);
+        } else if (c[index] == c1[index1]) {
+            return method2(c, c1, c2, index + 1, index1 + 1, index2);
+        } else {
+            return method2(c, c1, c2, index + 1, index1, index2 + 1);
         }
     }
 }
