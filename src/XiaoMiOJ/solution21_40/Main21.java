9f81d74bda19c464909cd7170c7dd4bb4105a922
magicya <794343815@qq.com>
2019-08-05 10:28:09 +0800

    89  N!

--- src/XiaoMiOJ/solution21_40/Main21.java
+++ src/XiaoMiOJ/solution21_40/Main21.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
  * @author: Ya
  * @create: 2019-05-17 23:25
  **/
+
+/**
+ * 出错了！！！！
+ */
 public class Main21 {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
@@ -18,6 +22,15 @@ public class Main21 {
         }
     }
 
+    /**
+     * 用例输入: "5,9,7,5,3,9,0,5,5,6,3,2,6,8 9,1,4,8 6"
+     * 期望输出: "999868"
+     * 你的输出: "999848"
+     * <p>
+     * 用例输入: "3,6,0,4,0,9,2,4,9,2,9,1,0 9,3,0,0,3,0,1,9,8,6,9,0,1,6,4,4,8,6 23"
+     * 期望输出: "99993100301986901644860"
+     * 你的输出: "99993100030198690164486"
+     */
     private static String method(String line) {
         String[] ss = line.split(" ");
         String[] ss1 = ss[0].split(",");
@@ -32,49 +45,77 @@ public class Main21 {
         for (int i = 0; i < a2.length; i++) {
             a2[i] = Integer.parseInt(ss2[i]);
         }
+        return method2(a1, a2, 0, 0, k);
+    }
 
+    /**
+     * 保持最后有足够的个数同时选择最大的一个
+     */
+    private static String method2(int[] a1, int[] a2, int index1, int index2, int k) {
         StringBuilder sb = new StringBuilder();
-        method(sb, a1, a2, -1, -1, k);
+        while (k > 0) {
+            int count1 = a1.length - index1;
+            int count2 = a2.length - index2;
+            //当前可选择的范围 (不包括当前下标)
+            int end1 = (k - count2 <= 0) ? a1.length : a1.length - (k - count2) + 1;
+            int end2 = (k - count1 <= 0) ? a2.length : a2.length - (k - count1) + 1;
+
+            //查找可选择范围内 最大值 和下标
+            int max1 = -1, i1 = -1;
+            for (int i = index1; i < end1; i++) {
+                if (a1[i] > max1) {
+                    max1 = a1[i];
+                    i1 = i;
+                }
+            }
+            int max2 = -1, i2 = -1;
+            for (int i = index2; i < end2; i++) {
+                if (a2[i] > max2) {
+                    max2 = a2[i];
+                    i2 = i;
+                }
+            }
+            if (max1 > max2) {
+                sb.append(max1);
+                index1 = i1 + 1;
+                k--;
+            } else if (max1 < max2) {
+                sb.append(max2);
+                index2 = i2 + 1;
+                k--;
+            } else {
+                sb.append(max1);
+                k--;
+                if (i1 == index1) {
+                    index1 = i1 + 1;
+                } else if (i2 == index2) {
+                    index2 = i2 + 1;
+                } else {
+                    String s1 = method2(a1, a2, i1 + 1, index2, k);
+                    String s2 = method2(a1, a2, index1, i2 + 1, k);
+                    if (compareString(s1, s2)) {
+                        return sb.append(s1).toString();
+                    } else {
+                        return sb.append(s2).toString();
+                    }
+                }
+            }
+        }
         return sb.toString();
     }
 
-    private static void method(StringBuilder sb, int[] a1, int[] a2, int index1, int index2, int k) {
-        if (k == 0) {
-            return;
-        }
-        //先确定能查找的范围
-        int end1;
-        int end2;
-        end1 = a1.length - (k - (a2.length - 1 - index2));
-        end1 = end1 > a1.length - 1 ? a1.length - 1 : end1;
-
-        end2 = a2.length - (k - (a1.length - 1 - index1));
-        end2 = end2 > a2.length - 1 ? a2.length - 1 : end2;
-
-        int max1 = Integer.MIN_VALUE;
-        int i1 = -1;
-        int max2 = Integer.MIN_VALUE;
-        int i2 = -1;
-
-        for (int i = index1 + 1; i <= end1; i++) {
-            if (a1[i] > max1) {
-                max1 = a1[i];
-                i1 = i;
+    /**
+     * 比较字符串
+     */
+    private static boolean compareString(String s1, String s2) {
+        int len = Integer.min(s1.length(), s2.length());
+        for (int i = 0; i < len; i++) {
+            if (s1.charAt(i) > s2.charAt(i)) {
+                return true;
+            } else if (s1.charAt(i) < s2.charAt(i)) {
+                return false;
             }
         }
-        for (int i = index2 + 1; i <= end2; i++) {
-            if (a2[i] > max2) {
-                max2 = a2[i];
-                i2 = i;
-            }
-        }
-
-        if (max1 >= max2) {
-            sb.append(max1);
-            method(sb, a1, a2, i1, index2, k - 1);
-        } else {
-            sb.append(max2);
-            method(sb, a1, a2, index1, i2, k - 1);
-        }
+        return s1.length() >= s2.length();
     }
 }
