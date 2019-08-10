194a8d47411908441ff2ad7b0c11891a727fe0f1
magicya <794343815@qq.com>
2019-08-10 21:22:08 +0800

    23  error

--- src/XiaoMiOJ/solution21_40/Main23.java
+++ src/XiaoMiOJ/solution21_40/Main23.java
@@ -1,5 +1,9 @@
 package XiaoMiOJ.solution21_40;
 
+import util.ArrayUtil;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -7,6 +11,9 @@ import java.util.Scanner;
  * @description: 找到第 N 个数字 II
  * @author: Ya
  * @create: 2019-05-19 23:59
+ * 用例输入: "1000"
+ * 期望输出: "4"
+ * 你的输出: "3"
  **/
 public class Main23 {
     public static void main(String args[]) {
@@ -18,22 +25,7 @@ public class Main23 {
         }
     }
 
-    private static char method(String line) {
-        long num = Long.parseLong(line);
-        long tag = 0;
-        long sum = 0;
-        long pre = 0;
-        while (sum < num) { //前n项和  (a1+an)n/2
-            tag++;
-            pre = sum;
-            sum += (Math.pow(10, tag) - Math.pow(10, tag - 1)) * tag;
-        }
-        long r = num - pre;
-        long a1 = (r - 1) / tag;
-        long a2 = (r - 1) % tag;
-
-        long a = (long) Math.pow(10, tag - 1) + a1;
-        String s = String.valueOf(a);
-        return s.charAt((int)a2);
+    private static int method(String line) {
+        return 0;
     }
 }
